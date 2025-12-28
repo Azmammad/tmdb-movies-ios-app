7d647e2f2f392ed26648b15e2cbd41809f2b13ae
Azmammad <azmammadjabrayilov@gmail.com>
2025-12-28 23:50:09 +0400

    initial commit

---
+++ TMDB Movies App/TMDB Movies App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMDB Movies App
+//
+//  Created by Əzi Cəbrayılov on 28.12.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

9c0ddedd4c18f04007053078c599eda88cecb443
Azmammad <azmammadjabrayilov@gmail.com>
2026-01-05 22:34:18 +0400

    addedHttpMethod enum for HTTP request methods

---
+++ TMDB Movies App/TMDB Movies App/Network/Core/HttpMethod.swift
@@ -0,0 +1,14 @@
+//
+//  HttpMethod.swift
+//  TMDB Movies App
+//
+//  Created by Əzi Cəbrayılov on 05.01.26.
+//
+
+enum HttpMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}

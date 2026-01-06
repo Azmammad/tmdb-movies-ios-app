a02782c76b37283a6018bd38ecc58c50ecfc64da
Azmammad <azmammadjabrayilov@gmail.com>
2026-01-06 14:09:10 +0400

    default watchlist builder,controller and viewModel

---
+++ TMDB Movies App/TMDB Movies App/Screens/WatchList/WatchListBuilder.swift
@@ -0,0 +1,14 @@
+//
+//  WatchListBuilder.swift
+//  TMDB Movies App
+//
+//  Created by Əzi Cəbrayılov on 06.01.26.
+//
+
+class WatchListBuilder {
+    func build() -> WatchListViewController {
+        let networkService = DefaultNetworkService()
+        let viewModel = WatchListViewModel(networkService: networkService)
+        return WatchListViewController(viewModel: viewModel)
+    }
+}

286763ea539e659e75d57567f0fa6061d67a98dc
Azmammad <azmammadjabrayilov@gmail.com>
2026-01-09 11:56:10 +0400

    deleted completion version change network service for async/await

--- TMDB Movies App/TMDB Movies App/Network/Core/NetworkService.swift
+++ TMDB Movies App/TMDB Movies App/Network/Core/NetworkService.swift
@@ -7,7 +7,10 @@
 import Foundation
 
 protocol NetworkService {
-    func request<T: Decodable>(_ endpoint: Endpoint, completion: @escaping (Result<T, NetworkError>) -> Void)
+    //func request<T: Decodable>(_ endpoint: Endpoint, completion: @escaping (Result<T, NetworkError>) -> Void)
+    
+    func request<T: Decodable>(_ endpoint: Endpoint) async throws -> T
+
 }
 
 final class DefaultNetworkService: NetworkService {
@@ -18,41 +21,34 @@ final class DefaultNetworkService: NetworkService {
         self.session = session
     }
     
-    func request<T: Decodable>(_ endpoint: any Endpoint, completion: @escaping (Result<T, NetworkError>) -> Void) {
-        let createRequest = endpoint.makeRequest()
-        
-        switch createRequest {
-        case .success(let request):
-            session.dataTask(with: request) { data, response, error in
-                
-                if let error {
-                    completion(.failure(.unknown(error)))
-                    return
-                }
-                
-                if let httpResponse = response as? HTTPURLResponse {
-                    let statusCode = httpResponse.statusCode
-                    
-                    guard (200...299).contains(statusCode) else {
-                        return completion(.failure(.serverError(statusCode: statusCode)))
-                    }
-                }
-                
-                guard let data else {
-                    return completion(.failure(.noData))
-                }
-                
-                do {
-                    let decodedData = try JSONDecoder().decode(T.self, from: data)
-                    completion(.success(decodedData))
-                } catch {
-                    completion(.failure(.decodingError))
-                }
-                
-            }.resume()
+    func request<T: Decodable>(_ endpoint: Endpoint) async throws -> T {
+            let request = endpoint.makeRequest()
             
-        case .failure(let error):
-            completion(.failure(error))
+            switch request {
+            case .success(let urlRequest):
+                do {
+                    let (data, response) = try await session.data(for: urlRequest)
+                    
+                    if let httpResponse = response as? HTTPURLResponse {
+                        let statusCode = httpResponse.statusCode
+                        
+                        guard (200...299).contains(statusCode) else {
+                            throw NetworkError.serverError(statusCode: statusCode)
+                        }
+                    }
+                    
+                    do {
+                        return try JSONDecoder().decode(T.self, from: data)
+                    } catch {
+                        throw NetworkError.decodingError
+                    }
+                    
+                } catch {
+                    throw error
+                }
+                
+            case .failure(let error):
+                throw error
+            }
         }
-    }
 }

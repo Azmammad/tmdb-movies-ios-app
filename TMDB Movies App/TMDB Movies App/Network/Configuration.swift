d509e0e160cb993b098b2d771f380f61bbecebfd
Azmammad <azmammadjabrayilov@gmail.com>
2026-01-10 20:21:21 +0400

    baseURL update

--- TMDB Movies App/TMDB Movies App/Network/Configuration.swift
+++ TMDB Movies App/TMDB Movies App/Network/Configuration.swift
@@ -8,5 +8,5 @@
 enum Configuration {
        static let tmdbAPIKey = "b6d1492e32dea26b8df0740167a74127"
        static let tmdbBearerToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNmQxNDkyZTMyZGVhMjZiOGRmMDc0MDE2N2E3NDEyNyIsIm5iZiI6MTc2NzYzNDA0NC4wMzUsInN1YiI6IjY5NWJmNDdjNTZmMDhiYmY1NDE3YmMzZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QD04VGqZd1L0NzXzj8EvELQflrQFJfV0OwInrZfrBGY"
-       static let baseURL = "https://api.themoviedb.org/3/"
+       static let baseURL = "https://api.themoviedb.org/"
 }

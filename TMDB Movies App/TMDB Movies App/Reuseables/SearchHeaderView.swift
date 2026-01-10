b99eb688dc71d58616463bae195288964483dedc
Azmammad <azmammadjabrayilov@gmail.com>
2026-01-11 02:03:32 +0400

    searchbar changed reusable ui component

---
+++ TMDB Movies App/TMDB Movies App/Reuseables/SearchHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchHeaderView.swift
+//  TMDB Movies App
+//
+//  Created by Əzi Cəbrayılov on 11.01.26.
+//
+
+import UIKit
+
+class SearchHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
